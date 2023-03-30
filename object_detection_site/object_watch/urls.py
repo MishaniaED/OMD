08d6d2c65710a4a940ae93401495128e6ff3afa9
Mikhail <misha.evkarpiev@mail.ru>
2023-03-30 11:24:35 +0300

    Prototype with authentication: still have problem with logo.png and favicon

--- object_detection_site/object_watch/urls.py
+++ object_detection_site/object_watch/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='object_watch-home'),
     path('about/', views.about, name='about-app'),
+    path('settings/', views.settings, name='settings'),
+    # path('update_camera_selection/', views.update_camera_selection, name='update_camera_selection')
 ]
--- object_detection_site/object_watch/views.py
+++ object_detection_site/object_watch/views.py
@@ -1,4 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+
+
+# def update_camera_selection(request):
+#     selected_value = request.POST.get('selected_value')
+#     # Обратиться к стриминговому сервису за видео и в случае успешного обращения отдать его в качестве ответа
+#     # return render(request, 'object_watch/home.html/camera-number', context)
+#     return JsonResponse({'success': True})
 
 
 def home(request):
@@ -10,3 +18,7 @@ def home(request):
 
 def about(request):
     return render(request, 'object_watch/about.html', {'title': 'Про нас'})
+
+
+def settings(request):
+    return render(request, 'object_watch/settings.html', {'title': 'Настройки'})
