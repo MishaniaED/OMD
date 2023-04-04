c87bcb265e0e0c9bc502315f6d4d79c75c4388e3
Mikhail <misha.evkarpiev@mail.ru>
2023-04-04 12:13:05 +0300

    Still have problem with logo.png and favicon. Forms with requests were made on the main page.

--- object_detection_site/object_watch/views.py
+++ object_detection_site/object_watch/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from .forms import ObjectRecognitionForm
+
+
+# Прописать функцию для обработки запроса на отображения видео на главной странице и странице с настройками
 
 
 def home(request):
-    context = {
-        'posts': 'some info'
-    }
-    return render(request, 'object_watch/home.html', context)
+    form = ObjectRecognitionForm()
+    if request.method == 'POST':
+        form = ObjectRecognitionForm(request.POST)
+        if form.is_valid():
+            frames = form.cleaned_data['frames']
+            camera = form.cleaned_data['camera']
+            # Здесь посылаем запрос на переключение камеры стриминнговому серверу
+    return render(request, 'object_watch/home.html', {'title': 'Главная', 'camera_form': form})
 
 
 def about(request):
