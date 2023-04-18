c2bedc13af79f7e9816aaa656ffd6ee247010915
MishaniaED <misha.evkarpiev@mail.ru>
2023-04-18 16:18:41 +0300

    Installed Apache kafka modul

--- object_detection_site/object_watch/forms.py
+++ object_detection_site/object_watch/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ObjectRecognitionForm(forms.Form):
-    camera = forms.ChoiceField(label='Выберите камеру:', choices=[('camera1', 'Camera 1')], required=False,
+    camera = forms.ChoiceField(label='Выберите камеру:', choices=[('camera1', 'Камера 1')], required=False,
                                widget=forms.Select(attrs={'class': 'form-control', 'id': 'camera-selector'}))
     frames = forms.BooleanField(label='Рамки вокруг распознаных объектов', required=False,
                                 widget=forms.CheckboxInput(attrs={'class': 'form-check-input', 'id': 'defaultCheck1'}))
