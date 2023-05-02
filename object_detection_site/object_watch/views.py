d1249c6a9817e4f53947c6225875a48067a6e04b
Mikhail <misha.evkarpiev@mail.ru>
2023-05-02 19:15:12 +0300

    requirements.txt updated

--- object_detection_site/object_watch/views.py
+++ object_detection_site/object_watch/views.py
@@ -1,3 +1,4 @@
+from django.conf.global_settings import MEDIA_ROOT
 from django.shortcuts import render
 from .forms import ObjectRecognitionForm
 
@@ -13,7 +14,7 @@ def home(request):
             frames = form.cleaned_data['frames']
             camera = form.cleaned_data['camera']
             # Здесь посылаем запрос на переключение камеры стриминнговому серверу
-    return render(request, 'object_watch/home.html', {'title': 'Главная', 'camera_form': form})
+    return render(request, 'object_watch/home.html', {'title': 'Главная', 'camera_form': form, 'media': MEDIA_ROOT})
 
 
 def about(request):
